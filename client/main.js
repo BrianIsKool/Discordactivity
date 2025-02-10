29efa5b1877b439d509a54d805367418aa63a98a
ivicodermobile <ivicodermobile@gmail.com>
2025-02-10 12:04:24 +0200

    dev

--- client/main.js
+++ client/main.js
@@ -1,9 +1,45 @@
 import './style.css'
 import rocketLogo from '/rocket.png'
+import { io } from "socket.io-client";
+
+//const socket = io('http://localhost:3000');
+const socket = io(':3000');
+
+console.log(socket);
+socket.on('pong', (msg) => {
+    console.log('pong')
+})
+
+socket.on('updates_fetched', (msg) => {
+    console.log('updates fetched', msg);
+})
+
+socket.emit('ping');
+socket.emit('fetch_updates', {last_tick: 12});
+//alert(1);
 
 document.querySelector('#app').innerHTML = `
   <div>
     <img src="${rocketLogo}" class="logo" alt="Discord" />
-    <h1>Hello, World123456!</h1>
+    <h1>Hello, World123!</h1>
+    <canvas id="canvas">
+    </canvas>
   </div>
 `;
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+ctx.fillStyle = "red";
+ctx.fillRect(0, 0, 150, 75);
+
+const arr = [];
+
+for (let i = 0; i < 10; i++) {
+    arr.push([]);
+    for (let j = 0; j < 10; j++) {
+       arr[i].push(0);
+    }
+}
+
+console.log(arr);
+alert(arr.length);
--- server/server.js
+++ server/server.js
@@ -1,13 +1,194 @@
 import express from "express";
 import dotenv from "dotenv";
 import fetch from "node-fetch";
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
+import cors from 'cors'
+
 dotenv.config({ path: "../.env" });
 
 const app = express();
-const port = 3001;
+const port = 3000;
+const ALLOWED_ORIGIN = "http://localhost:3000";
 
-// Allow express to parse JSON bodies
-app.use(express.json());
+const server = createServer(app);
+const io = new Server(server, {
+  cors: ALLOWED_ORIGIN,
+  serveClient: false
+})
+
+const clients = {};
+const width = 7;
+const height = 5;
+const ar = [];
+const users = new Map();
+const states = [];
+let current_tick = 0;
+
+//console.log(users.size);
+function init_ar() {
+  for (let y = 0; y < height; y++) {
+    ar[y] = [];
+    for (let x = 0; x < width; x++) {
+      ar[y][x] = 0;
+    }
+  }
+}
+
+function init_test_users() {
+
+  users.set(1, {
+    id: 1,
+    x: 0,
+    y: Math.floor(height / 2),
+    dx: 1,
+    dy: 0
+  });
+
+  users.set(2, {
+    id: 2,
+    x: Math.floor(width),
+    y: Math.floor(height / 2),
+    dx: -1,
+    dy: 0
+  });
+
+}
+
+new_round();
+
+function new_round() {
+  init_ar();
+  init_test_users();
+  console.log(users);
+  setInterval(new_tick, 2000);
+}
+
+function new_tick() {
+
+  console.log('new_tick');
+
+  update_users();
+  console.log(users);
+
+  add_state();
+  console.log(states);
+
+}
+
+function update_users() {
+  for (let [id, user] of users) {
+    update_user(user);
+  }
+}
+
+function update_user(user) {
+  //console.log(id);
+  //console.log(user);
+  user.x += user.dx;
+  user.y += user.dy;
+  //console.log(user);
+
+  const tick_users = [];
+
+  const tick = {
+
+  }
+
+}
+
+function add_state() {
+
+  const state = {
+    users: []
+  };
+
+  for (let [id, user] of users) {
+    state.users.push({
+      id: user.id,
+      x: user.x,
+      y: user.y
+    })
+  }
+
+  //states.push
+
+
+}
+/*
+setInterval(() => {
+  new_tick();
+  // runs every 2 seconds
+}, 2000);
+*/
+
+
+/*
+app.use(
+    cors({
+      origin: ALLOWED_ORIGIN
+    })
+)
+*/
+
+app.use(express.json())// Allow express to parse JSON bodies
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
+});
+
+io.on('connection', (socket) => {
+
+  console.log('User %s connected: ', socket.id);
+
+  clients[socket.id] = {
+    id: socket.id,
+    socket: socket,
+    x: 0,
+    y: height / 2,
+    dx: 1,
+    dy: 0
+  };
+
+  const keys = Object.keys(clients);
+
+  keys.forEach(key => {
+    console.log(key);
+    //console.log(`${key} : ${obj[key]}`);
+  });
+
+  /*
+  for (let client_id in clients) {
+    //console.log(client_id);
+
+    if (clients.hasOwnProperty(client_id)) {
+      console.log(clients[client_id]);
+      //console.log(`${key} : ${obj[key]}`)
+    }
+  }
+
+   */
+
+
+  //console.log(clients);
+  //console.log(socket.id);
+  //console.log(socket);
+  socket.on('ping', (msg) => {
+    console.log('ping: ' + msg);
+    socket.emit('pong');
+  });
+
+  socket.on('fetch_updates', (msg) => {
+
+    console.log('fetch_updates: ' + JSON.stringify(msg));
+    socket.emit('updates_fetched', 'updates');
+  });
+
+  socket.on('disconnect', () => {
+    delete clients[socket.id];
+    console.log('User disconnected: ' + socket.id);
+    console.log(clients);
+  });
+});
 
 app.post("/api/token", async (req, res) => {
   
@@ -32,6 +213,12 @@ app.post("/api/token", async (req, res) => {
   res.send({access_token});
 });
 
-app.listen(port, () => {
+app.get("/index", async (req, res) => {
+
+  // Return the access_token to our client as { access_token: "..."}
+  res.send('Hello world');
+});
+
+server.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
